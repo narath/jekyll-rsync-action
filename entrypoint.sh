747533aea1322098ce371803377c480baf3bc783
joeversoza <joeversoza@penguin>
2020-06-05 23:20:59 -0400

    fix environment variable syntax error aws-

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
 # actions mounts $GITHUB_WORKSPACE, so available in our container...
-BUILD_DIR = $GITHUB_WORKSPACE/_site
+BUILD_DIR=$GITHUB_WORKSPACE/_site
 
 # concat destination variables for rsync DEST
 DEST=$DEST_USERNAME@$DEST_HOST/$DEST_PATH 
 
-SSH_KEY_PATH = $HOME/.ssh/id_rsa
+SSH_KEY_PATH=$HOME/.ssh/id_rsa
 
 # assuming site source present in GITHUB_WORKSPACE, build into default _site
 cd $GITHUB_WORKSPACE
